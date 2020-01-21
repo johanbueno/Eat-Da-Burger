8189642b220bc1179fd1a6aafba23776363f00b9
Johan Bueno <johan.s.bueno@gmail.com>
2020-01-20 22:12:48 -0500

    structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('CHILLI BURGER', TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('CRISPY CHICKEN BURGER', TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('GRILLED CHICKEN BURGER', TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('BACON BURGER', TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('SPICY JALAPENO BURGER', TRUE);
