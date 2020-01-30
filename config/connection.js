7e555abe62156e2a432abc0d2d3f8c5a8dfbc4db
Johan Bueno <johan.s.bueno@gmail.com>
2020-01-30 18:17:02 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 
-if (process.env.jawsdb_url) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }   else {
   connection = mysql.createConnection({
